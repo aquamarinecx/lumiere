d36d3c65fdbf30ac717e6decb88b5e5765a294ef
warren yun <xxwarrenhdxx@gmail.com>
2021-11-13 12:13:12 -0500

    Somewhat render mdx onto page (thanks anthony!)

--- pages/company/blog/index.js
+++ pages/company/blog/index.js
@@ -1,12 +1,31 @@
+import Head from 'next/head';
+import { getMDXComponent } from 'mdx-bundler/client';
 import Layout from '@components/layouts/Layout';
-
-export default function Blogs() {
+import { getAllSlugs } from '@lib/mdxBundler';
+// error
+export default function Blogs({ slugs }) {
   return (
-    <div className="">
+    <>
+      <Head>
+        <title>Blog - Lumiere</title>
+      </Head>
+
       <h1 className="text-gray-100 heading-primary">Lumiere Blogs</h1>
-    </div>
+      {slugs.map((slug) => (
+        <h1>{slug.replace(/\.mdx/, '')}</h1>
+      ))}
+    </>
   );
 }
+
+export const getStaticProps = async () => {
+  const slugs = await getAllSlugs(['company', 'blog']);
+  return {
+    props: {
+      slugs,
+    },
+  };
+};
 
 Blogs.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
