1fff6f9147767c2e446a31ca10a21b331ace630e
Arash <47273556+arashnrim@users.noreply.github.com>
2021-11-14 12:39:30 +0800

    Replace header in Editor page with EditorHeader

--- pages/editor.js
+++ pages/editor.js
@@ -1,47 +1,23 @@
-import { useState, useRef } from 'react';
-import Head from 'next/head';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
-import toast, { Toaster } from 'react-hot-toast';
 import MDXEditor from '@components/editor/MDXEditor';
-import Header from '@components/layouts/Header';
-import { useBeforeunload } from 'react-beforeunload';
+import EditorHeader from '@components/layouts/EditorHeader';
 import { useXdm } from '@lib/xdm';
+import { useSession } from 'next-auth/react';
+import Head from 'next/head';
+import { useState } from 'react';
+import { useBeforeunload } from 'react-beforeunload';
+import { Toaster } from 'react-hot-toast';
 
 export default function Editor() {
   const [collapsed, setCollapsed] = useState(false);
-  const [title, setTitle] = useState('');
-  const [slug, setSlug] = useState('');
-  const titleInput = useRef(null);
-  const router = useRouter();
   const { data: session } = useSession();
 
   const [state, setConfig] = useXdm({
     value: '',
   });
 
-  const saveDraft = async () => {
-    const content = state.value;
-    try {
-      const body = { title, content, slug };
-      await fetch('/api/post/create', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(body),
-      });
-      await router.push('/me/drafts');
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const showUntitledError = () => {
-    titleInput.current.focus();
-    toast.error('You must set a title to your publication before saving.');
-  };
-
+  // NOTE: Remove when autosave is implemented
   useBeforeunload((event) => {
-    if (session && (title !== '' || state.value !== '')) {
+    if (session && state.value !== '') {
       event.preventDefault();
     }
   });
@@ -51,17 +27,10 @@ export default function Editor() {
       <Head>
         <title>Editor — Lumiere</title>
       </Head>
-
-      <Header
-        pageType="editor"
-        title={title}
-        titleInput={titleInput}
-        saveDraft={saveDraft}
-        showUntitledError={showUntitledError}
-        setTitle={setTitle}
-        setSlug={setSlug}
+      <EditorHeader
         collapsed={collapsed}
         setCollapsed={setCollapsed}
+        state={state}
       />
       <MDXEditor state={state} setConfig={setConfig} collapsed={collapsed} />
       <Toaster position="bottom-left" />
