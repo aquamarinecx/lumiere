97c929eccee3f5b8d1ab1be88c4ef98ced2a4ff9
warren yun <xxwarrenhdxx@gmail.com>
2021-11-21 11:15:24 -0500

    Create team dir

--- pages/company/team.js
+++ pages/company/team.js
@@ -1,12 +1,33 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 import Layout from '@components/layouts/Layout';
+
+const members = [{}];
 
 export default function Team() {
   return (
-    <div>
-      <h1>Team</h1>
-    </div>
+    <>
+      <Head>
+        <title>Team - Lumiere</title>
+      </Head>
+
+      <h1 className="mb-2 text-gray-100 heading-primary">Team</h1>
+      <p className="mb-12 text-gray-300 text-md">
+        Thanks to all our amazing team members for making Lumiere possible!
+      </p>
+      <section className="grid grid-cols-3">
+        <div>
+          <Image
+            src="/images/team/warren.png"
+            alt="warren"
+            width={200}
+            height={200}
+            className="rounded-full"
+          />
+        </div>
+      </section>
+    </>
   );
 }
 
