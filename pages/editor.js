25d44583a5bf3c0ea26f1fdf61af528865032b09
Arash <47273556+arashnrim@users.noreply.github.com>
2021-11-24 10:27:09 +0800

    Add prompt to reload Editor on resize

--- pages/editor.js
+++ pages/editor.js
@@ -3,17 +3,60 @@ import EditorHeader from '@components/layouts/EditorHeader';
 import { useXdm } from '@lib/xdm';
 import { useSession } from 'next-auth/react';
 import Head from 'next/head';
-import { useState } from 'react';
+import { useState, useEffect, useLayoutEffect, useRef } from 'react';
 import { useBeforeunload } from 'react-beforeunload';
-import { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
+import { useMediaQuery } from 'react-responsive';
 
 export default function Editor() {
   const [collapsed, setCollapsed] = useState(false);
   const { data: session } = useSession();
+  const isMobile = useMediaQuery({ maxWidth: 889 });
 
   const [state, setConfig] = useXdm({
     value: '',
   });
+
+  const usePrevious = (value) => {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  };
+
+  let previousIsMobile = usePrevious(isMobile);
+  if (previousIsMobile === undefined) {
+    previousIsMobile = isMobile;
+  }
+
+  useEffect(() => {
+    if (isMobile !== previousIsMobile) {
+      toast(
+        () => (
+          <span>
+            It appears that your device width has changed. If the Editor appears
+            to be broken, click{' '}
+            <button
+              type="button"
+              className="underline"
+              onClick={() => window.location.reload()}
+            >
+              here
+            </button>{' '}
+            to reload.{' '}
+            <b>
+              Be sure to {session ? 'save' : 'copy'} your draft to prevent
+              anything from being lost.
+            </b>
+          </span>
+        ),
+        {
+          duration: 10000,
+        }
+      );
+    }
+  }, [isMobile, previousIsMobile]);
 
   // NOTE: Remove when autosave is implemented
   useBeforeunload((event) => {
