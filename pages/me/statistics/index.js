befefc4c4aa11d570e877cb753f668ce52e93dfc
warren yun <wyun2006@gmail.com>
2022-01-03 02:03:07 -0500

    i cannot style

--- pages/me/statistics/index.js
+++ pages/me/statistics/index.js
@@ -1,5 +1,6 @@
 import Layout from '@components/layouts/Layout';
 import Head from 'next/head';
+import Image from 'next/dist/client/image';
 import { getSession, useSession, signIn } from 'next-auth/react';
 import prisma from '@lib/prisma';
 
@@ -18,8 +19,27 @@ export default function Statistics({ stats }) {
       <Head>
         <title>Statistics - Lumiere</title>
       </Head>
-      <h1>Statistics</h1>
-      <h1>{stats.username}</h1>
+      <h1 className="mb-8">Statistics</h1>
+      <div className="flex flex-row items-start justify-center w-7/12 p-16 bg-gray-800 border border-gray-800 rounded-3xl">
+        <div className="mr-auto">
+          <Image
+            src={stats.pfp}
+            alt="user profile picture"
+            width={200}
+            height={200}
+            className="inline rounded-full"
+          />
+          <h3 className="mt-6">{stats.username}</h3>
+        </div>
+        <div className="flex flex-col items-center justify-start text-center">
+          <h3>{stats.name}</h3>
+          <p className="mb-4 text-gray-300">{stats.email}</p>
+          <div className="w-full h-full p-6 text-left bg-gray-600 border border-gray-600 rounded-3xl">
+            <h4 className="text-gray-100">{stats.published} publication(s)</h4>
+            <h4 className="text-gray-100">{stats.drafts} draft(s)</h4>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
