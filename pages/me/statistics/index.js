42be9af8065b0d908150d33c4ae76bfd8b76e4a0
warren yun <wyun2006@gmail.com>
2022-01-02 19:26:12 -0500

    Data fetching go brr

--- pages/me/statistics/index.js
+++ pages/me/statistics/index.js
@@ -1,16 +1,66 @@
 import Layout from '@components/layouts/Layout';
 import Head from 'next/head';
+import { getSession, useSession, signIn } from 'next-auth/react';
+import prisma from '@lib/prisma';
 
-export default function Statistics() {
+export default function Statistics({ stats }) {
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      signIn();
+    },
+  });
+
+  if (status === 'loading') return null;
+
   return (
     <>
       <Head>
         <title>Statistics - Lumiere</title>
       </Head>
       <h1>Statistics</h1>
+      <h1>{stats.username}</h1>
     </>
   );
 }
+
+export const getServerSideProps = async ({ req, res }) => {
+  const session = await getSession({ req });
+
+  if (!session) {
+    res.statusCode = 403;
+    return { props: { data: [] } };
+  }
+
+  // const data = await prisma.post.findMany({
+  //   where: {
+  //     author: { username: session.user.username },
+  //   },
+  //   select: {
+  //     id: true,
+  //     published: true,
+  //   },
+  // });
+
+  const userData = await prisma.user.findUnique({
+    where: {
+      username: session.user.username,
+    },
+    include: {
+      posts: true,
+    },
+  });
+
+  const stats = {
+    name: userData.name,
+    email: userData.email,
+    username: userData.username,
+  };
+
+  return {
+    props: { stats },
+  };
+};
 
 Statistics.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
