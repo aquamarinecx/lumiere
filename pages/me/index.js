4a9f22b19968d1d48c015c36b8fae2f77f484f25
warren yun <wyun2006@gmail.com>
2022-01-04 22:39:07 -0500

    Change profile site structure

--- pages/me/index.js
+++ pages/me/index.js
@@ -2,6 +2,7 @@ import Layout from '@components/layouts/Layout';
 import Head from 'next/head';
 import Image from 'next/dist/client/image';
 import { getSession, useSession, signIn } from 'next-auth/react';
+import Publication from '@components/ui/Publication';
 import prisma from '@lib/prisma';
 
 export default function Statistics({ stats }) {
@@ -17,27 +18,19 @@ export default function Statistics({ stats }) {
   return (
     <>
       <Head>
-        <title>Statistics - Lumiere</title>
+        <title>Profile - Lumiere</title>
       </Head>
-      <h1 className="mb-8">Statistics</h1>
-      <div className="flex flex-row items-start justify-center w-7/12 p-16 bg-gray-800 border border-gray-800 rounded-3xl">
-        <div className="mr-auto">
+      <div className="flex flex-row">
+        <div className="flex flex-col justify-center">
           <Image
             src={stats.pfp}
             alt="user profile picture"
-            width={200}
-            height={200}
+            width={275}
+            height={275}
             className="inline rounded-full"
           />
-          <h3 className="mt-6">{stats.username}</h3>
-        </div>
-        <div className="flex flex-col items-center justify-start text-center">
-          <h3>{stats.name}</h3>
-          <p className="mb-4 text-gray-300">{stats.email}</p>
-          <div className="w-full h-full p-6 text-left bg-gray-600 border border-gray-600 rounded-3xl">
-            <h4 className="text-gray-100">{stats.published} publication(s)</h4>
-            <h4 className="text-gray-100">{stats.drafts} draft(s)</h4>
-          </div>
+          <h2 className="mt-5 text-3xl">{stats.username}</h2>
+          <h3 className="text-2xl text-gray-400">{stats.name}</h3>
         </div>
       </div>
     </>
@@ -68,7 +61,7 @@ export const getServerSideProps = async ({ req, res }) => {
     if (post.published) publishedCount += 1;
     else draftCount += 1;
   });
-
+  
   const stats = {
     name: userData.name,
     email: userData.email,
@@ -76,6 +69,7 @@ export const getServerSideProps = async ({ req, res }) => {
     pfp: userData.image,
     published: publishedCount,
     drafts: draftCount,
+    publications: userData.posts,
   };
 
   return {
