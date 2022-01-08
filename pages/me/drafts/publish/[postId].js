119fe006310afe80e3565209ef0c96c18d1d9c37
warren yun <wyun2006@gmail.com>
2022-01-08 00:53:06 -0500

    Much simpler schema for tags

--- pages/me/drafts/publish/[postId].js
+++ pages/me/drafts/publish/[postId].js
@@ -1,17 +1,42 @@
 import { useRouter } from 'next/router';
-import Image from 'next/image';
+import Head from 'next/head';
 import Layout from '@components/layouts/Layout';
+import prisma from '@lib/prisma';
 
 export default function Publish({ post }) {
   return (
     <>
-      <h1>hello</h1>
+      <Head>
+        <title>Publish - Lumiere</title>
+      </Head>
+      <h1>Preview</h1>
     </>
   );
 }
 
 export const getServerSideProps = async ({ params }) => {
-  const id = params.postId;
+  const { postId } = params;
+
+  const post = await prisma.post.findUnique({
+    where: {
+      id: postId,
+    },
+    select: {
+      id: true,
+      title: true,
+      slug: true,
+      content: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  post.createdAt = String(post.createdAt);
+  post.updatedAt = String(post.updatedAt);
+
+  return {
+    props: { post },
+  };
 };
 
 Publish.getLayout = function getLayout(page) {
