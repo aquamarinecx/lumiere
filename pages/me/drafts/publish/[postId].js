3ee6bf90d481a80409e1ca2f1ae3c74a80bf8c1f
warren yun <wyun2006@gmail.com>
2022-01-08 22:54:24 -0500

    Add tag button adds stuff!

--- pages/me/drafts/publish/[postId].js
+++ pages/me/drafts/publish/[postId].js
@@ -1,11 +1,12 @@
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import Head from 'next/head';
 import Layout from '@components/layouts/Layout';
 import prisma from '@lib/prisma';
 
 export default function Publish({ post }) {
   const router = useRouter();
-  const tags = [];
+  const [tags, setTag] = useState([]);
 
   const publishPost = async (slug) => {
     try {
@@ -34,17 +35,26 @@ export default function Publish({ post }) {
       >
         Publish
       </button>
-      <h1 className="mb-4">{post.title}</h1>
+      <h2 className="mb-4">{post.title}</h2>
       <input
         id="desc"
         className="w-full p-3 mb-3 text-gray-100 bg-gray-800 outline-none rounded-xl"
         placeholder="Enter a small caption/description"
       />
-      <h1>{tags[0]}</h1>
+      <div className="flex flex-row w-full py-2 mb-4">
+        {tags.map((tag) => (
+          <p
+            key={tag}
+            className="inline px-2 py-1 mr-4 text-purple-400 border border-purple-400 rounded-xl"
+          >
+            {tag}
+          </p>
+        ))}
+      </div>
       <button
         type="button"
         className="px-2.5 py-1 mb-4 text-gray-300 duration-200 border border-gray-400 rounded-xl hover:text-pink-600 hover:border-pink-600"
-        onClick={() => tags.push('LMAOOO')}
+        onClick={() => setTag([...tags, 'AAAH'])}
       >
         Add tags
       </button>
