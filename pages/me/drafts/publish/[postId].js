4053a89ec4c39f751c9df234eb325773a501ef9e
warren yun <wyun2006@gmail.com>
2022-01-11 13:25:10 -0500

    Rewrite html and nest into form

--- pages/me/drafts/publish/[postId].js
+++ pages/me/drafts/publish/[postId].js
@@ -12,9 +12,10 @@ export default function Publish({ post }) {
   const router = useRouter();
   const [tags, setTag] = useState([]);
 
-  const publishPost = async (slug) => {
+  const publishPost = async (event, slug, title, desc, tagList) => {
+    event.preventDefault();
     try {
-      const body = { slug };
+      const body = { slug, title, desc, tagList };
       await fetch('/api/post/publish', {
         method: 'PATCH',
         headers: { 'Content-Type': 'application/json' },
@@ -41,70 +42,75 @@ export default function Publish({ post }) {
         <title>Publish - Lumiere</title>
       </Head>
 
-      <button
-        type="button"
-        className="p-3.5 mb-4 text-3xl text-gray-200 duration-200 border border-gray-300 rounded-xl hover:text-green-600 hover:border-green-600"
-        onClick={() => publishPost(post.slug)}
-      >
-        Publish
-      </button>
-      <input
-        id="title"
-        className="w-full p-4 mb-3 text-3xl text-gray-100 bg-gray-800 outline-none rounded-xl"
-        placeholder="Title"
-        autoComplete="off"
-        defaultValue={post.title}
-      />
-      <input
-        id="desc"
-        className="w-full p-3 mb-3 text-gray-100 bg-gray-800 outline-none rounded-xl"
-        placeholder="Enter a small caption/description"
-        autoComplete="off"
-      />
-      <div className="flex flex-row items-center justify-start">
-        <Image
-          src={post.author.image}
-          width={35}
-          height={35}
-          className="rounded-full "
-          alt={`${post.author.username}s profile picture`}
-        />
-        <p className="ml-3">Published by {post.author.username}</p>
-      </div>
-      <div className="py-2 mb-4">
-        {tags.map((tag) => (
-          <div
-            key={tag}
-            className="inline w-full px-4 py-2 mr-4 text-purple-400 duration-200 border border-purple-400 hover:border-pink-600 rounded-xl hover:text-pink-600 hover:cursor-pointer"
-          >
-            <div className="inline-block my-3">
-              <button
-                onClick={() => {
-                  setTag(tags.filter((tempTag) => tempTag !== tag));
-                }}
-                type="button"
-                className="mr-4 text-red-700 duration-200 hover:text-red-400"
-              >
-                X
-              </button>
-              <p className="inline">{tag}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-      <form onSubmit={addTag} className="pb-6 mb-6 border-b-2 border-gray-500">
-        <input
-          id="tagInput"
-          className="p-3 mb-3 mr-2 text-gray-100 bg-gray-800 outline-none rounded-xl"
-          placeholder="Enter a tag"
-          autoComplete="off"
-        />
+      <form onSubmit={publishPost}>
         <button
           type="submit"
-          className="px-2.5 py-1 mb-4 text-gray-300 duration-200 border border-gray-400 rounded-xl hover:text-pink-600 hover:border-pink-600"
+          className="p-3.5 mb-4 text-3xl text-gray-200 duration-200 border border-gray-300 rounded-xl hover:text-green-600 hover:border-green-600"
+          onClick={() => publishPost(post.slug)}
         >
-          Add tag
+          Publish
         </button>
+        <input
+          id="title"
+          className="w-full p-4 mb-3 text-3xl text-gray-100 bg-gray-800 outline-none rounded-xl"
+          placeholder="Title"
+          autoComplete="off"
+          defaultValue={post.title}
+        />
+        <input
+          id="desc"
+          className="w-full p-3 mb-3 text-gray-100 bg-gray-800 outline-none rounded-xl"
+          placeholder="Enter a small caption/description"
+          autoComplete="off"
+        />
+        <div className="flex flex-row items-center justify-start my-2">
+          <Image
+            src={post.author.image}
+            width={35}
+            height={35}
+            className="rounded-full "
+            alt={`${post.author.username}s profile picture`}
+          />
+          <p className="ml-3">Published by {post.author.username}</p>
+        </div>
+        <div className="py-2 mb-4">
+          {tags.map((tag) => (
+            <div
+              key={tag}
+              className="inline w-full px-4 py-2 mr-4 text-purple-400 duration-200 border border-purple-400 hover:border-pink-600 rounded-xl hover:text-pink-600 hover:cursor-pointer"
+            >
+              <div className="inline-block my-3">
+                <button
+                  onClick={() => {
+                    setTag(tags.filter((tempTag) => tempTag !== tag));
+                  }}
+                  type="button"
+                  className="mr-4 text-red-700 duration-200 hover:text-red-400"
+                >
+                  X
+                </button>
+                <p className="inline">{tag}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <form
+          onSubmit={addTag}
+          className="pb-6 mb-6 border-b-2 border-gray-500"
+        >
+          <input
+            id="tagInput"
+            className="p-3 mb-3 mr-2 text-gray-100 bg-gray-800 outline-none rounded-xl"
+            placeholder="Enter a tag"
+            autoComplete="off"
+          />
+          <button
+            type="submit"
+            className="px-2.5 py-1 mb-4 text-gray-300 duration-200 border border-gray-400 rounded-xl hover:text-pink-600 hover:border-pink-600"
+          >
+            Add tag
+          </button>
+        </form>
       </form>
       <Component components={MDXComponents} />
     </>
